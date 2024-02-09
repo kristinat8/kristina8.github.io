c9e7970f17979b6e9d9cc623c563fe3b7bdf5589
kristinat8 <159520360+kristinat8@users.noreply.github.com>
2024-02-09 15:40:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('current-year').textContent = new Date().getFullYear();
+
+document.getElementById('nav-toggle').addEventListener('click', function(){
+    var navMenu = document.getElementById('nav-menu');
+    if(navMenu.style.display === 'block') {
+        navMenu.style.display = 'none';
+    } else {
+        navMenu.style.display = 'block';
+    }
+})
+
+/* 平滑滚动到页面锚点 */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
